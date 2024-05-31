5b3a881d5132d5e2017a07d42871c06a958c5c8a
Wesley Carneiro <wesley1995.silva@gmail.com>
2024-05-31 17:34:55 -0300

    fix

--- mlp/data/file_handler.py
+++ mlp/data/file_handler.py
@@ -9,7 +9,6 @@ class FileHandler:
     def __init__(self, dir_path: str):
         self.__vectors = self.__read_file_vectors(dir_path)
         self.__chars = self.__read_file_chars(dir_path)
-        self.target_dict = self.__read_file_targets(dir_path)
 
     @property
     def vectors(self) -> list[list[int]]:
@@ -18,10 +17,6 @@ class FileHandler:
     @property
     def chars(self) -> list[str]:
         return self.__chars
-    
-    @property
-    def targets(self) -> dict[str, list[int]]:
-        return self.target_dict
 
     def __line_format_vector(self, content: str) -> list[int]:
         content_list_str = content.replace(" ", "").replace("\n", "").split(",")
@@ -46,19 +41,3 @@ class FileHandler:
             for line in file:
                 chars.append(line.replace("\n", ""))
         return chars
-    
-    def __line_format_target(self, target_dict: dict, line: str) -> None:
-        line = line.split("    ")
-        vector = []
-        char = line[1].replace("\n","")
-        for i in line[0].split(" "):
-            vector.append(int(i))
-        target_dict[char] = vector
-    
-    def __read_file_targets(self, dir_path: str) -> dict[str, list[int]]:
-        file_path = os.path.join(dir_path, "targets.txt")
-        target_dict = {}
-        with open(file_path, "r") as file:
-            for line in file:
-                self.__line_format_target(target_dict, line)
-        return target_dict
